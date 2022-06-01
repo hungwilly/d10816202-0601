729d42f77051b1cca7b0a9d045edff71dabb854e
hungwilly <d10816202@gs.takming.edu.tw>
2022-06-01 17:05:38 +0800

    fix: init portal card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,29 @@
         人員清單
       </h1>
     </v-col>
+    <v-col md="4" cols="12">
+      <v-card height="150px">
+      <div>
+        <h3 class="primary--text"><strong>王小明</strong></h3>
+        <h4 class="gray--text">男</h4>
+      </div>
+
+      <div>
+          <span class="gray--text">
+            2021062011493428
+          </span>
+      </div>
+
+    <div>
+        <v-icon left color="secondary">
+          mdi-gmail
+        </v-icon>
+        <span class="secondary--text">
+          Takmig001@takming.edu.tw
+        </span>
+    </div>
+      </v-card>
+    </v-col>
   </v-row>
 </v-container>
 </template>
